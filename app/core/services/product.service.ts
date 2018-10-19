a86833ec4d204175b16be8c0996dd7ccb9c299bf
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-10-19 11:24:03 +0530

    fixed tslint issues

--- app/core/services/product.service.ts
+++ app/core/services/product.service.ts
@@ -35,51 +35,49 @@ export class ProductService {
     return this.http
       .get<Product>(
         `api/v1/products/${id}?data_set=large`
-      )
+      );
   }
 
   getProducts(pageNumber: 1): Observable<Array<Product>> {
     return this.http
       .get<Array<Product>>(
         `api/v1/products?q[s]=avg_rating+desc&page=${pageNumber}&per_page=20&data_set=small`
-      )
+      );
   }
 
-  getproductsByKeyword(keyword: string): Observable<Array<Product>> {
+  getproductsByKeyword(keyword: string): Observable<any> {
     return this.http
-      .get<Array<Product>>(
+      .get(
         `api/v1/products?${keyword}&per_page=20&data_set=small`
-      )
+      );
   }
 
   getProductDetail(id: string) {
-    return this.http.get(`api/v1/products/${id}?data_set=large`)
+    return this.http.get(`api/v1/products/${id}?data_set=large`);
   }
 
   getRelatedProducts(productId: number): Observable<Array<Product>> {
     return this.http
-      .get<Array<Product>>(`api/products/${productId}/relations`)
+      .get<Array<Product>>(`api/products/${productId}/relations`);
   }
 
-  getProductReviews(products): Observable<any> {    
+  getProductReviews(products): Observable<any> {
     return this.http.get(`products/${products}/reviews`);
   }
  
   submitReview(productId: any, params: any) {
     return this.http.post(`products/${productId}/reviews`, params).pipe(
       map(
-        success => {
+        (success) => {
           this.success = success;
-          if (this.success.type === 'info') {
-           
+          if (this.success.type === "info") {
             return this.success.type;
           } else {
-            
             return this.success.type;
           }
         },
-        error => {
-          this.error = error;         
+        (error) => {
+          this.error = error;
           return this.error.type;
         }
       )
