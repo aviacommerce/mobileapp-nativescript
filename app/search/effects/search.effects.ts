a8800702c5d1a08baa9bc64e3f11113a7af4d7f3
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-10-19 11:24:03 +0530

    fixed tslint issues

--- app/search/effects/search.effects.ts
+++ app/search/effects/search.effects.ts
@@ -1,23 +1,16 @@
-import { map, switchMap } from 'rxjs/operators';
-import { Product } from '~/core/models/product';
-import { SearchActions } from '../action/search.actions';
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
-import { ProductService } from '~/core/services/product.service';
-import { Action } from '@ngrx/store';
+import { Injectable } from "@angular/core";
+import { Actions, Effect } from "@ngrx/effects";
+import { Action } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map, switchMap } from "rxjs/operators";
+import { ProductService } from "~/core/services/product.service";
+import { SearchActions } from "../action/search.actions";
 
 @Injectable()
 export class SearchEffects {
-  constructor(
-    private actions$: Actions,
-    private productService: ProductService,
-    private searchActions: SearchActions,
-
-  ) { }
 
   @Effect()
-  GetProductsByKeyword$: Observable<Action> = this.actions$
+  getProductsByKeyword$: Observable<Action> = this.actions$
     .ofType(SearchActions.GET_PRODUCTS_BY_KEYWORD)
     .pipe(
       switchMap((action: any) =>
@@ -27,4 +20,11 @@ export class SearchEffects {
         this.searchActions.getProductsByKeywordSuccess({ products, pagination })
       )
     );
+
+  constructor(
+    private actions$: Actions,
+    private productService: ProductService,
+    private searchActions: SearchActions
+
+  ) { }
 }
