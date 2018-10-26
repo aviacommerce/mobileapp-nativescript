9895323c15b9e03f849759ed6dd035f72bddf07c
jaypal412 <39181626+jaypal412@users.noreply.github.com>
2018-10-26 12:17:51 +0530

    Order success page (#15)
    ## Why?
    
    **Feature:**   
    1.Order success page UI.

--- app/checkout/order-success/order-response.module.ts
+++ app/checkout/order-success/order-response.module.ts
@@ -2,7 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { SharedModule } from "~/shared/shared.module";
-import { OrderResponeComponent } from "./order-respone.component";
+import { OrderResponseComponent } from "./order-response.component";
 
 @NgModule({
     imports: [
@@ -11,7 +11,7 @@ import { OrderResponeComponent } from "./order-respone.component";
         SharedModule
     ],
     declarations: [
-        OrderResponeComponent
+        OrderResponseComponent
     ],
 
     providers: [
